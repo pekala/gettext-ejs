9362ca820b007500ce08921663c0ff9787771f05
smhg <sam@strictlyphp.com>
2016-10-09 10:46:02 +0200

    update deps

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import test from 'ava';
 
 test('default', t => {
-    t.ok((new Parser()).keywordSpec.gettext.length > 0,
+    t.truthy((new Parser()).keywordSpec.gettext.length > 0,
         'should have default keyword spec when none is passed');
 });
 
@@ -13,9 +13,9 @@ test.cb('singluar', t => {
 
         var result = (new Parser()).parse(data);
         t.is(typeof result, 'object');
-        t.ok('inside block' in result);
-        t.ok('inside block inverse' in result);
-        t.ok('word \\"escaped, word\\", with comma' in result);
+        t.truthy('inside block' in result);
+        t.truthy('inside block inverse' in result);
+        t.truthy('word \\"escaped, word\\", with comma' in result);
         t.is(Object.keys(result).length, 15);
         t.is(result['Image description'].line.length, 2);
         t.end();
